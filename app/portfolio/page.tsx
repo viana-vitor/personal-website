c461b513653b667373068d1059332f023e199b4f
Vitor Viana <86685331+viana-vitor@users.noreply.github.com>
2023-04-25 20:58:28 +0000

    .

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -3,9 +3,14 @@ import Link from "next/link";
 
 export default function PortfolioPage() {
   return (
-    <div className="grid grid-cols-3">
-      <div className="flex flex-col">
-        <a>
+    <>
+    <div className="text-2xl mb-3">
+      Projects
+    </div>
+    <div className="grid grid-cols-3 gap-2 ">
+      <div className="flex flex-col border rounded-md p-2 bg-neutral-100">
+        <a href="https://github.com/viana-vitor/Handie"
+        target="_blank">
           <div className="flex justify-center">
             <Image
               src={"/images/handie.png"}
@@ -27,5 +32,6 @@ export default function PortfolioPage() {
         </a>
       </div>
     </div>
+    </>
   );
 }
