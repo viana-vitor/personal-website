eabb365968534baba7d7b7fe55f1486c32d3e5d1
Vitor Viana <86685331+viana-vitor@users.noreply.github.com>
2023-05-02 18:12:28 -0700

    portfolio page style

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,3 +1,8 @@
 export default function BlogPage() {
-  return <div>Blog</div>;
+  return (
+    <>
+      <div className="text-3xl mb-3 font-serif font-bold">Blog</div>
+      <div className="">No posts to show at the moment.</div>
+    </>
+  );
 }
--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -5,30 +5,28 @@ export default function PortfolioPage() {
   return (
     <>
       <div className="text-3xl mb-3 font-serif font-bold">Projects</div>
-      <div className="grid grid-cols-1 sm:grid-cols-3 gap-2 ">
-        <div className="flex flex-col border rounded-md p-2 bg-neutral-100 dark:bg-neutral-800">
-          <a href="https://github.com/viana-vitor/Handie" target="_blank">
-            <div className="flex justify-center bg-neutral-200 rounded-lg shadow-md py-2">
-              <Image
-                src={"/images/handieicon.svg"}
-                alt="Project page"
-                width={128}
-                height={128}
-                className="w-32 h-32"
-              />
-            </div>
-            <div className="mt-2">
-              <h1 className="text-2xl font-semibold font-serif text-neutral-800 dark:text-neutral-200">
-                Handie
-              </h1>
-              <p className="mt-2 text-neutral-800 dark:text-neutral-200">
-                Handie is an app for general contractors to manage clients,
-                projects, and estimates. Created in Python using PySide6.
-              </p>
-            </div>
-          </a>
+      <a href="https://github.com/viana-vitor/Handie" target="_blank">
+        <div className="grid sm:grid-cols-2 gap-2">
+          <div className="flex justify-center shrink mt-4">
+            <Image
+              src={"/images/handieicon.svg"}
+              alt="Project page"
+              width={128}
+              height={128}
+              className="w-52 h-52 drop-shadow-lg"
+            />
+          </div>
+          <div className="flex flex-col justify-center mt-4">
+            <h1 className="text-2xl font-semibold font-serif text-neutral-800 dark:text-neutral-200">
+              Handie
+            </h1>
+            <p className="mt-2 text-neutral-800 dark:text-neutral-200">
+              Handie is an app for general contractors to manage clients,
+              projects, and estimates. Created in Python using PySide6.
+            </p>
+          </div>
         </div>
-      </div>
+      </a>
     </>
   );
 }
